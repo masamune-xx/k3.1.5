4695835b057e89a0ed9e4ed334b160bb140a385e
masamune <emopanda1337@gmail.com>
2023-11-16 14:38:29 +0700

    -cors.disable

--- src/main/java/com/xpl/k315/configs/WebSecurityConfig.java
+++ src/main/java/com/xpl/k315/configs/WebSecurityConfig.java
@@ -23,7 +23,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .cors().disable()
                 .authorizeRequests()
                 .antMatchers("/").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/login").permitAll()
